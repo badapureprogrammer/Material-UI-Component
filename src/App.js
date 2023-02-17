126587310fce0a7ee512dc64e6a63059c8bc809f
Arun Badapure <arunbadapure00000>
2023-02-17 11:27:39 +0530

    add AutoComplete

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import MuiTypography from "./component/Typography/MuiTypography";
 import MuiAutoComplete from "./component/AutoComplete/MuiAutoComplete";
 
 function App() {
+
+  const skills = ["HTML5", "CSS3", "Bootstrap", "Javascript", "SASS"];
+
   return (
     <div className="Mui-component">
       <MuiTypography color={"error"} variant={"h5"} weight={300}>I want to Become Good Programer</MuiTypography>
-      <MuiAutoComplete boxWidth={"300px"}/>
+      <MuiAutoComplete boxWidth={"350px"} size={"small"} menuList={skills} isMultiple={true} label={"Subject"}/>
     </div>
   );
 }
--- src/component/AutoComplete/MuiAutoComplete.jsx
+++ src/component/AutoComplete/MuiAutoComplete.jsx
@@ -2,27 +2,28 @@ import React, { useState } from "react";
 import { Stack, Autocomplete, TextField } from "@mui/material";
 
 const MuiAutoComplete = (props) => {
-  const skills = ["HTML5", "CSS3", "Bootstrap", "Javascript", "SASS"];
+  const { boxWidth, size, isMultiple, menuList, label } = props;
 
-  const { boxWidth } = props;
-
-  const [value, setValue] = useState();
+  const [value, setValue] = useState([]);
 
   const handleChange = (event, newValue) => {
-    setValue(newValue);
+    const value = event.target.innerText;
+    setValue(value);
   };
 
   return (
     <Stack spacing={2} width={boxWidth}>
-      <Autocomplete
-        options={skills}
-        renderInput={(params) => {
-          <TextField {...params} label="skills" />;
-        }}
-        value={value}
-        onChange={handleChange}
+      <Autocomplete size={size} multiple={isMultiple} options={menuList}
+        renderInput={(params) => <TextField {...params} label={label} />}
+        value={value} onChange={handleChange}
       />
     </Stack>
   );
 };
+
 export default MuiAutoComplete;
+
+// Use This Example
+// const skills = ["HTML5", "CSS3", "Bootstrap", "Javascript", "SASS"]; Pass the value
+// <MuiAutoComplete boxWidth={"350px"} size={"small"} menuList={skills} isMultiple={true} label={"Subject"}/>
+
