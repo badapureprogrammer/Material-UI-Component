8ec366afb340bebb401cebc24c305b336bda4355
Arun Badapure <arunbadapure00000>
2023-02-23 21:46:52 +0530

    add Changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
-import React from "react";
+import MuiTypography from "./component/Typography/MuiTypography";
 
 function App() {
-  return (
-      <div className="Mui-component">
 
-      </div>
+  return (
+    <div className="Mui-component">
+      <MuiTypography color={"error"} variant={"h5"} weight={300}>
+        I want to Become Good Programer
+      </MuiTypography>
+    </div>
   );
 }
 
